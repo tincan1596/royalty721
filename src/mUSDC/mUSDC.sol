9a82a18f6311175419d080d53eb304c83bb0320b
tincan1596 <sahilatwal2003@gmail.com>
2025-08-04 14:58:37 +0000

    erc20 permit changes

--- src/mUSDC/mUSDC.sol
+++ src/mUSDC/mUSDC.sol
@@ -1,19 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
-import {ERC20Permit} from "solmate/tokens/ERC20Permit.sol";
-import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract mUSDC is ERC20Permit, Ownable {
-    constructor(address _owner)
-        ERC20("Mock USDC", "mUSDC", 6)
-        ERC20Permit("Mock USDC")
-        Ownable(_owner)
-    {}
+contract mUSDC is ERC20, Ownable {
+    constructor(address _owner) ERC20("Mock USDC", "mUSDC", 6) Ownable(_owner) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
