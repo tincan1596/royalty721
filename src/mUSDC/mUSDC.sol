04f9610155695d1b16670c79fce48dbf3fea7414
tincan1596 <sahilatwal2003@gmail.com>
2025-08-04 14:35:18 +0000

    mUSDC implimentation

--- src/mUSDC/mUSDC.sol
+++ src/mUSDC/mUSDC.sol
@@ -1,2 +1,13 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+
+contract mUSDC is ERC20, Ownable {
+    constructor(address _owner) ERC20("Mock USDC", "mUSDC", 6) Ownable(_owner) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
