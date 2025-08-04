50ebeb004c762fa62adfef04925edcbfa597c019
tincan1596 <sahilatwal2003@gmail.com>
2025-08-04 16:32:31 +0000

    final mUSDC

--- src/mUSDC/mUSDC.sol
+++ src/mUSDC/mUSDC.sol
@@ -5,10 +5,14 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract mUSDC is ERC20, Ownable {
+    event minted(address indexed to, uint256 amount);
+
     constructor(address _owner) ERC20("Mock USDC", "mUSDC", 6) Ownable(_owner) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
+        require(to != address(0), "Cannot mint to zero address");
         _mint(to, amount);
+        emit minted(to, amount);
     }
 }
 
