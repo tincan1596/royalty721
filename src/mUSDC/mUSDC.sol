14e2d068ef94d02e1fcf76427048ae9ac7605696
tincan1596 <sahilatwal2003@gmail.com>
2025-08-05 15:27:49 +0000

    mUSDC contrusctor changes

--- src/mUSDC/mUSDC.sol
+++ src/mUSDC/mUSDC.sol
@@ -7,7 +7,9 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 contract mUSDC is ERC20, Ownable {
     event minted(address indexed to, uint256 amount);
 
-    constructor(address _owner) ERC20("Mock USDC", "mUSDC", 6) Ownable(_owner) {}
+    constructor(address _owner) ERC20("Mock USDC", "mUSDC", 6) Ownable() {
+        _transferOwnership(_owner);
+    }
 
     function mint(address to, uint256 amount) external onlyOwner {
         require(to != address(0), "Cannot mint to zero address");
