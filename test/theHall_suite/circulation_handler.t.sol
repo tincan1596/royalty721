1aa6ed393481ae26ed255d7c5990ba20166d4abe
tincan1596 <sahilatwal2003@gmail.com>
2025-08-17 19:50:21 +0000

    handler final

--- test/theHall_suite/circulation_handler.t.sol
+++ test/theHall_suite/circulation_handler.t.sol
@@ -16,7 +16,6 @@ interface IStoken {
         view
         returns (address receiver, uint256 royaltyAmount);
     function ownerOf(uint256 tokenId) external view returns (address);
-    function safeTransferFrom(address from, address to, uint256 tokenId) external;
     function getApproved(uint256 tokenId) external view returns (address);
     function isApprovedForAll(address owner, address operator) external view returns (bool);
     function approve(address to, uint256 tokenId) external;
@@ -24,6 +23,13 @@ interface IStoken {
 }
 
 interface IHall {
+    struct Listing {
+        address seller;
+        uint256 price;
+    }
+
+    function listings(uint256 id) external view returns (Listing memory);
+
     function createListing(uint256 id, uint256 price) external;
     function cancelListing(uint256 id) external;
     function buyToken(uint256 id, uint256 expectedPrice) external;
@@ -37,52 +43,26 @@ contract HallHandler {
     IHall public immutable hall;
     address public immutable stokenOwner;
 
-    address[] public actors;
-
-    mapping(uint256 => bool) public minted;
-    mapping(uint256 => address) public lastSeller;
-    mapping(uint256 => uint256) public listedPrice;
-
     uint256 public sumPrice;
     uint256 public sumRoyalty;
     uint256 public sumRevenue;
 
-    constructor(address _usdc, address _stoken, address _hall, address _stokenOwner, address[] memory _actors) {
+    constructor(address _usdc, address _stoken, address _hall, address _stokenOwner) {
         usdc = IMUSDC(_usdc);
         stoken = IStoken(_stoken);
         hall = IHall(_hall);
         stokenOwner = _stokenOwner;
-        for (uint256 i; i < _actors.length; i++) {
-            actors.push(_actors[i]);
-        }
     }
 
-    function actorsLen() public view returns (uint256) {
-        return actors.length;
-    }
-
-    function _actor(uint256 seed) internal view returns (address) {
-        return actors[seed % actors.length];
-    }
-
-    function _distinct(uint256 seed, address avoid) internal view returns (address) {
-        if (actors.length < 2 || avoid == address(0)) return _actor(seed);
-        address a = _actor(seed);
-        return a == avoid ? _actor(seed + 1) : a;
-    }
-
-    function mintAndList(uint256 sellerSeed, uint256 id, uint256 rawPrice, uint256 approveModeSeed) public {
-        uint256 tokenId = id % 10;
-        address seller = _actor(sellerSeed);
+    function mintAndList(uint256 seed, uint256 rawId, uint256 rawPrice, uint256 approveModeSeed) public {
+        address seller = _addr(seed);
+        uint256 tokenId = rawId % 10;
         uint256 price = _price(rawPrice);
 
-        if (!minted[tokenId]) {
-            vm.startPrank(stokenOwner);
-            stoken.mint(seller, tokenId);
-            vm.stopPrank();
-            minted[tokenId] = true;
-        }
+        // Mint if not owned by anyone
+        _ensureMinted(seller, tokenId);
 
+        // Approve hall if needed
         if (!stoken.isApprovedForAll(seller, address(hall)) && stoken.getApproved(tokenId) != address(hall)) {
             vm.startPrank(seller);
             if (approveModeSeed % 2 == 0) {
@@ -93,63 +73,68 @@ contract HallHandler {
             vm.stopPrank();
         }
 
+        // List
         if (stoken.ownerOf(tokenId) == seller) {
             vm.startPrank(seller);
-            try hall.createListing(tokenId, price) {
-                lastSeller[tokenId] = seller;
-                listedPrice[tokenId] = price;
-            } catch {}
+            try hall.createListing(tokenId, price) {} catch {}
             vm.stopPrank();
         }
     }
 
-    function cancel(uint256 callerSeed, uint256 id) public {
-        uint256 tokenId = id % 10;
-        address caller = _actor(callerSeed);
-        vm.startPrank(caller);
-        try hall.cancelListing(tokenId) {
-            listedPrice[tokenId] = 0;
-        } catch {}
-        vm.stopPrank();
-    }
+    function buy(uint256 seed, uint256 rawId) public {
+        address buyer = _addr(seed);
+        uint256 tokenId = rawId % 10;
 
-    function buy(uint256 buyerSeed, uint256 id) public {
-        uint256 tokenId = id % 10;
-        uint256 price = listedPrice[tokenId];
-        if (price == 0) return;
+        IHall.Listing memory lst = hall.listings(tokenId);
+        if (lst.price == 0 || lst.seller == address(0) || lst.seller == buyer) return;
 
-        address seller = lastSeller[tokenId];
-        address buyer = _distinct(buyerSeed, seller);
+        uint256 price = lst.price;
 
+        // fund buyer
         usdc.mint(buyer, price);
         vm.startPrank(buyer);
         usdc.approve(address(hall), price);
         vm.stopPrank();
 
+        // compute royalty
         (, uint256 royalty) = stoken.royaltyInfo(tokenId, price);
         uint256 revenue = price - royalty;
 
+        // buy
         vm.startPrank(buyer);
         try hall.buyToken(tokenId, price) {
             sumPrice += price;
             sumRoyalty += royalty;
             sumRevenue += revenue;
-            listedPrice[tokenId] = 0;
         } catch {}
         vm.stopPrank();
     }
 
-    function mintListBuy(uint256 sellerSeed, uint256 buyerSeed, uint256 id, uint256 rawPrice, uint256 approveModeSeed)
-        external
-    {
-        mintAndList(sellerSeed, id, rawPrice, approveModeSeed);
-        buy(buyerSeed, id);
+    function mintListBuy(uint256 sSeed, uint256 bSeed, uint256 rawId, uint256 rawPrice, uint256 approveSeed) external {
+        mintAndList(sSeed, rawId, rawPrice, approveSeed);
+        buy(bSeed, rawId);
+    }
+
+    // === Internals ===
+
+    function _ensureMinted(address to, uint256 tokenId) internal {
+        // If nobody owns the tokenId yet, mint it
+        try stoken.ownerOf(tokenId) returns (address owner) {
+            if (owner != address(0)) return;
+        } catch {
+            vm.startPrank(stokenOwner);
+            stoken.mint(to, tokenId);
+            vm.stopPrank();
+        }
+    }
+
+    function _addr(uint256 seed) internal pure returns (address) {
+        return address(uint160(uint256(keccak256(abi.encode(seed)))));
     }
 
     function _price(uint256 x) internal pure returns (uint256) {
-        uint256 minP = 1e6;
-        uint256 maxP = 1_000_000e6;
-        uint256 r = x % (maxP - minP + 1);
-        return r + minP;
+        uint256 minP = 10e6;
+        uint256 maxP = 1_000e6;
+        return (x % (maxP - minP + 1e6)) + minP;
     }
 }
